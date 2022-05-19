6c8e4bdfbbd6737f29ccb949cfc446066e964bf5
Binjita <binjita.baral@gmail.com>
2022-05-19 12:44:22 -0500

    working on data ingestion

---
+++ src/main/java/io/javabrains/ipldashboard/data/BatchConfig.java
@@ -0,0 +1,73 @@
+package io.javabrains.ipldashboard.data;
+
+import javax.activation.DataSource;
+
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.item.database.BeanPropertyItemSqlParameterSourceProvider;
+import org.springframework.batch.item.database.JdbcBatchItemWriter;
+import org.springframework.batch.item.database.builder.JdbcBatchItemWriterBuilder;
+import org.springframework.batch.item.file.FlatFileItemReader;
+import org.springframework.batch.item.file.builder.FlatFileItemReaderBuilder;
+import org.springframework.batch.item.file.mapping.BeanWrapperFieldSetMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+
+import io.javabrains.ipldashboard.model.MatchGame;
+
+@Configuration
+@EnableBatchProcessing
+
+public class BatchConfig {
+
+    private final String[] FIELD_NAMES = new String [] {
+        "id", "city", "date", "player_of_match", "venue", "neutral_venue", "team1", "team2", "toss_winner", 
+        "toss_decision", "winner", "result", "result_margin", "eliminator", "method", "umpire1", "umpire2"
+    };
+
+  @Autowired
+  public JobBuilderFactory jobBuilderFactory;
+
+  @Autowired 
+  public StepBuilderFactory stepBuilderFactory;
+
+  @Bean //this is to read all the data from csv file
+public FlatFileItemReader<MatchInput> reader() { 
+  return new FlatFileItemReaderBuilder<MatchInput>().name("MatchGameItemReader")
+    .resource(new ClassPathResource("IPL Matches.csv")).delimited()
+    .names(FIELD_NAMES)
+    .fieldSetMapper(new BeanWrapperFieldSetMapper<MatchInput>() {
+    {
+      setTargetType(MatchInput.class);
+    }
+      }).build();
+}
+
+@Bean  //this part will process the input data(processor)
+public MatchDataProcessor processor() {
+  return new MatchDataProcessor();
+}
+
+@Bean //this is to write csv file contents into database
+public JdbcBatchItemWriter<MatchGame> writer(DataSource dataSource) { 
+  return new JdbcBatchItemWriterBuilder<MatchGame>()
+    .itemSqlParameterSourceProvider(new BeanPropertyItemSqlParameterSourceProvider<>())
+    .sql("INSERT INTO matchGame (id, city, date, player_of_match, venue, team1, team2, toss_winner, toss_decision, match_winner, result, result_margin, umpire1, umpire2)" 
+    + " VALUES (:id,:city,:date,:playerOfMatch, :venue,:Team1,:Team2,:tossWinner, :tossDecision,:matchWinner,:result, :reultMargin, :umpire1, :umpire2" )
+    //values should be same as MatchGame class variables
+    .build();
+}
+
+
+
+
+
+
+
+
+}
+
+
